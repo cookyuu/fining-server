36bef3fad80ebfe220e45c96c1e7956f4dbd72e7
limhaekyu <limk9500@gmail.com>
2024-04-29 00:27:09 +0900

    Initial commit

---
+++ src/main/java/com/hklim/finingserver/FiningServerApplication.java
@@ -0,0 +1,13 @@
+package com.hklim.finingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiningServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiningServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hklim/finingserver/FiningServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hklim.finingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiningServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
